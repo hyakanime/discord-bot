09a0dddd2029f5f89cb11315a7439e6e8efe9338
dem0nx309 <41202006+dem0nx309@users.noreply.github.com>
2022-11-28 18:30:23 +0100

    Ajout commande agenda

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Partials, Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
 const { token , roleBeta, channelBeta, channelBienvenue, roleMembre, rolePatchNotes, roleIos, roleAndroid, roleSite , roleBonPlan, roleGenshin} = require('./config.json');
 const {phrases} = require('./bienvenue.json');
+const fetch = require("node-fetch");
 const client = new Client(
     {
         intents: [
@@ -198,7 +199,73 @@ client.on('interactionCreate', async (interaction) => {
             }
 
             break;
+        case 'agenda':
 
+            await interaction.deferReply();
+            let date = new Date();
+            date.setHours(0, 0, 0, 0);
+            timestampAjrd = Date.parse(date);
+            date.setDate(date.getDate() + 1);
+            timestampDemain = Date.parse(date);
+        
+            let headersList = {
+              Accept: "*/*",
+              "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+            };
+        
+            let response = await fetch(
+              "https://api.hyakanime.fr/episode/sortie-hebdo/" +
+                timestampAjrd +
+                "/" +
+                timestampDemain,
+              {
+                method: "GET",
+                headers: headersList,
+              }
+            );
+        
+            let data = await response.text();
+            var result = JSON.parse(data);
+            var i = 0;
+            var b = 0;
+            var nom = [];
+            var episode = [];
+            var timestamp = [];
+            while (i < result.length) {
+              if (result[i].displayCalendar == true) {
+                nom[b] = result[i].animeTitle;
+                episode[b] = result[i].title;
+                const heure = new Date(result[i].timestamp);
+                if (heure.getMinutes() == 0) {
+                  timestamp[b] = heure.getHours() + "h00" ;
+                } else {
+                  timestamp[b] = heure.getHours() + "h" + heure.getMinutes();
+                }
+                b++;
+              }
+              i++;
+            }
+            i = 0;
+            const exampleEmbed = new EmbedBuilder()
+              .setColor(0x0099ff)
+              .setTitle("Agenda du Jour")
+              .setURL("https://hyakanime.fr/agenda")
+              .setAuthor({
+                name: "hyakanime",
+                iconURL:
+                  "https://www.hyakanime.fr/static/media/appLogo.7fac0ec4359bda8ccf0f.png",
+                url: "https://hyakanime.fr",
+              })
+              .setTimestamp();
+              i=0
+              while (i < nom.length) {
+                exampleEmbed.addFields({name: timestamp[i], value: nom[i].slice(0, 43)+" - "+episode[i], inline: false})
+              i++;
+              }
+                
+              
+            await interaction.editReply({ embeds: [exampleEmbed] });
+            break;
     }
 
 
