0c9ec3495dfcb5b68d962f7ea7b26c4c39c4acad
dem0nx309 <41202006+dem0nx309@users.noreply.github.com>
2022-12-09 15:36:56 +0100

    Ajout option retrait role
    Quand on clique sur le bouton pour avoir tel role et que l'on l'as déja il nous le retire

--- index.js
+++ index.js
@@ -341,39 +341,81 @@ client.on('interactionCreate', async (interaction) => {
     let role = {};
     switch (interaction.customId) {
         case 'verifyButton':
-            interaction.reply({ content: 'Vous venez de recevoir le rôle <@&845340817828479026>', ephemeral: true });
             role = interaction.guild.roles.cache.find(role => role.id == roleMembre);
+            if(interaction.member.roles.cache.find(r => r.id === roleMembre)){
+                interaction.reply({ content: 'Le rôle <@&'+roleMembre+'> est bien retiré', ephemeral: true });
+                await interaction.member.roles.remove(role);
+            }
+            else{
+            interaction.reply({ content: 'Vous venez de recevoir le rôle <@&'+roleMembre+'>', ephemeral: true });
             await interaction.member.roles.add(role);
+            }
             break;
         case 'role-patchnote':
-            interaction.reply({ content: 'Vous venez de recevoir le rôle <@&1012649092343668857>', ephemeral: true });
             role = interaction.guild.roles.cache.find(role => role.id == rolePatchNotes);
+            if(interaction.member.roles.cache.find(r => r.id === rolePatchNotes)){
+                interaction.reply({ content: 'Le rôle <@&'+rolePatchNotes+'> est bien retiré', ephemeral: true });
+                await interaction.member.roles.remove(role);
+            }
+            else{
+            interaction.reply({ content: 'Vous venez de recevoir le rôle <@&'+rolePatchNotes+'>', ephemeral: true });
             await interaction.member.roles.add(role);
+            }
             break;
         case 'role-ios':
-            interaction.reply({ content: 'Vous venez de recevoir le rôle <@&949075801637281812>', ephemeral: true });
             role = interaction.guild.roles.cache.find(role => role.id == roleIos);
+            if(interaction.member.roles.cache.find(r => r.id === roleIos)){
+                interaction.reply({ content: 'Le rôle <@&'+roleIos+'> est bien retiré', ephemeral: true });
+                await interaction.member.roles.remove(role);
+            }
+            else{
+            interaction.reply({ content: 'Vous venez de recevoir le rôle <@&'+roleIos+'>', ephemeral: true });
             await interaction.member.roles.add(role);
+            }
             break;
         case 'role-android':
-            interaction.reply({ content: 'Vous venez de recevoir le rôle <@&949360235011776533>', ephemeral: true });
             role = interaction.guild.roles.cache.find(role => role.id == roleAndroid);
+            if(interaction.member.roles.cache.find(r => r.id === roleAndroid)){
+                interaction.reply({ content: 'Le rôle <@&'+roleAndroid+'> est bien retiré', ephemeral: true });
+                await interaction.member.roles.remove(role);
+            }
+            else{
+            interaction.reply({ content: 'Vous venez de recevoir le rôle <@&'+roleAndroid+'>', ephemeral: true });
             await interaction.member.roles.add(role);
+            }
             break;
         case 'role-siteweb':
-            interaction.reply({ content: 'Vous venez de recevoir le rôle <@&949075846113669131>', ephemeral: true });
             role = interaction.guild.roles.cache.find(role => role.id == roleSite);
+            if(interaction.member.roles.cache.find(r => r.id === roleSite)){
+                interaction.reply({ content: 'Le rôle <@&'+roleSite+'> est bien retiré', ephemeral: true });
+                await interaction.member.roles.remove(role);
+            }
+            else{
+            interaction.reply({ content: 'Vous venez de recevoir le rôle <@&'+roleSite+'>', ephemeral: true });
             await interaction.member.roles.add(role);
+            }
             break;
         case 'role-bonplan':
-            interaction.reply({ content: 'Vous venez de recevoir le rôle <@&948247271290572850>', ephemeral: true });
             role = interaction.guild.roles.cache.find(role => role.id == roleBonPlan);
+            if(interaction.member.roles.cache.find(r => r.id === roleBonPlan)){
+                interaction.reply({ content: 'Le rôle <@&'+roleBonPlan+'> est bien retiré', ephemeral: true });
+                await interaction.member.roles.remove(role);
+            }
+            else{
+            interaction.reply({ content: 'Vous venez de recevoir le rôle <@&'+roleBonPlan+'>', ephemeral: true });
             await interaction.member.roles.add(role);
+            }
             break;
         case 'role-genshin':
-            interaction.reply({ content: 'Vous venez de recevoir le rôle <@&948247223894954074>', ephemeral: true });
             role = interaction.guild.roles.cache.find(role => role.id == roleGenshin);
+            if(interaction.member.roles.cache.find(r => r.id === roleGenshin)){
+                interaction.reply({ content: 'Le rôle <@&'+roleGenshin+'> est bien retiré', ephemeral: true });
+                await interaction.member.roles.remove(role);
+            }
+            else{
+            interaction.reply({ content: 'Vous venez de recevoir le rôle <@&'+roleGenshin+'>', ephemeral: true });
             await interaction.member.roles.add(role);
+            }
             break;
         default:
             interaction.reply({ content: 'Une erreur est survenue, contactez <@&245604480278593537>', ephemeral: true });
