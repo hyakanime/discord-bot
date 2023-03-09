8a7961b35f3c6bf5a44254b47613681ac4e488ac
dem0nx309 <41202006+dem0nx309@users.noreply.github.com>
2023-03-09 16:25:33 +0100

    Correction bug commande bêta
    Création d'un nouveau client pour envoyer le message dans le salon admin , il est surement possible de le faire de manière plus propre a voir

--- commands/beta.js
+++ commands/beta.js
@@ -1,13 +1,16 @@
-const { Partials, Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, AttachmentBuilder,SlashCommandBuilder} = require("discord.js");
-const {roleBeta, channelBeta} = require('../config.json');
-const client = new Client({ intents:[
+const {Client, GatewayIntentBits, EmbedBuilder, SlashCommandBuilder} = require("discord.js");
+const {token, roleBeta, channelBeta} = require('../config.json');
+const client = new Client({
+  intents: [
+  GatewayIntentBits.DirectMessages,
   GatewayIntentBits.Guilds,
   GatewayIntentBits.GuildBans,
   GatewayIntentBits.GuildMessages,
   GatewayIntentBits.MessageContent,
   GatewayIntentBits.GuildMembers,
-]
-});
+],}
+);
+client.login(token);
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -30,8 +33,7 @@ module.exports = {
         )
     ),
 
-  async execute(interaction, message) {
-    
+  async execute(interaction) {
     var email = interaction.options.get("email").value;
     var system = interaction.options.get("système").value;
     if (/^[a-zA-Z0-9_.]+@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}$/.test(email)) {
@@ -64,9 +66,8 @@ module.exports = {
           { name: "email", value: email, inline: false }
         )
         .setColor("#3d67ff");
-
-        client.channels.cache.get(channelBeta).send({ content: '', embeds: [embedbetaAdmin] })
-
+        const channel = await client.channels.fetch(channelBeta);
+        channel.send ({ content: '', embeds: [embedbetaAdmin] });
 
       await interaction.reply({
         content: "",
@@ -80,4 +81,4 @@ module.exports = {
       });
     }
   },
-}; //ReferenceError: Client is not defined
+};
