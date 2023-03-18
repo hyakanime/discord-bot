34c4ce4e347af39a6a26a6a1901e754eea3c56b7
dem0nx309 <41202006+dem0nx309@users.noreply.github.com>
2023-03-18 16:18:38 +0100

    Ajout de l'application Id dans le deploy

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require('discord.js');
-const { token } = require('./config.json');
+const { token , applicationId } = require('./config.json');
 const fs = require('node:fs');
 
 const commands = [];
@@ -22,7 +22,7 @@ const rest = new REST({ version: '10' }).setToken(token);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationCommands("978823077959983154"),
+			Routes.applicationCommands(applicationId),
 			{ body: commands },
 		);
 
