f9e92caa9b3d988e39355f3ffda446a41f0192a0
dem0nx309 <41202006+dem0nx309@users.noreply.github.com>
2023-05-07 23:31:22 +0200

    Rework Agenda
    Petit rework de l'agenda en passant par la bibliothéque de dayjs qui simplifie la manipulation de date et évite les erreurs

--- commands/agenda.js
+++ commands/agenda.js
@@ -1,85 +1,66 @@
-const { SlashCommandBuilder,EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const fetch = require("node-fetch");
+const dayjs = require('dayjs')
+require('dayjs/locale/fr')
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("agenda")
-    .setDescription("Fournit l'agenda du jour."),
+    data: new SlashCommandBuilder()
+        .setName("agenda")
+        .setDescription("Fournit l'agenda du jour."),
 
-  async execute(interaction) {
-  
-    await interaction.deferReply();
-    let date = new Date();
-    if (date.getTimezoneOffset() == 0)
-    {
-       var changementTimestamp = 3600000;
-       var changementHeure = 1;
-    }
-    else
-    {
-        var changementTimestamp = 0;
-        var changementHeure = 0;
-    }
-    date.setHours(0, 0, 0, 0);
-    timestampAjrd = Date.parse(date)-changementTimestamp;
-    date.setDate(date.getDate() + 1);
-    timestampDemain = Date.parse(date)-changementTimestamp;
-    let headersList = {
-      Accept: "*/*",
-      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-    };
-    let response = await fetch(
-      "https://api.hyakanime.fr/episode/sortie-hebdo/" +
-        timestampAjrd +
-        "/" +
-        timestampDemain,
-      {
-        method: "GET",
-        headers: headersList,
-      }
-    );
+    async execute(interaction) {
+        await interaction.deferReply();
+        dayjs.locale("fr")
+        var now = dayjs().hour(0).minute(0).second(0);
+        var après = now.add((1), "day");
+        now = now.valueOf();
+        après = après.valueOf();
+        console.log(now)
+        console.log(après)
+        let response = await fetch(
+            "https://api.hyakanime.fr/episode/sortie-hebdo/" +
+            now +
+            "/" +
+            après,
+            {
+                method: "GET",
+            }
+        );
 
-    let data = await response.text();
-    var result = JSON.parse(data);
-    var i = 0;
-    var b = 0;
-    var nom = [];
-    var episode = [];
-    var timestamp = [];
-    while (i < result.length) {
-      if (result[i].displayCalendar == true) {
-        nom[b] = result[i].animeTitle;
-        episode[b] = result[i].title;
-        const heure = new Date(result[i].timestamp);
-        
-        if (heure.getMinutes() == 0) {
-          timestamp[b] = heure.getHours()+changementHeure + "h00" ;
-        } else {
-          timestamp[b] = heure.getHours()+changementHeure + "h" + heure.getMinutes();
+        let data = await response.text();
+        var result = JSON.parse(data);
+        var i = 0;
+        var b = 0;
+        var nom = [];
+        var episode = [];
+        var timestamp = [];
+        while (i < result.length) {
+            if (result[i].displayCalendar == true) {
+                nom[b] = result[i].animeTitle;
+                episode[b] = result[i].title;
+                timestamp[b] = dayjs(result[i].timestamp).format("H:mm");
+                b++;
+            }
+            i++;
         }
-        b++;
-      }
-      i++;
+        i = 0;
+        const exampleEmbed = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle("Agenda du Jour")
+            .setURL("https://hyakanime.fr/agenda")
+            .setAuthor({
+                name: "hyakanime",
+                iconURL:
+                    "https://www.hyakanime.fr/static/media/appLogo.7fac0ec4359bda8ccf0f.png",
+                url: "https://hyakanime.fr",
+            })
+            .setTimestamp();
+        i = 0
+        while (i < nom.length) {
+            exampleEmbed.addFields({ name: timestamp[i], value: nom[i].slice(0, 43) + " - " + episode[i], inline: false })
+            i++;
+        }
+
+        await interaction.editReply({ embeds: [exampleEmbed] });
     }
-    i = 0;
-    const exampleEmbed = new EmbedBuilder()
-      .setColor(0x0099ff)
-      .setTitle("Agenda du Jour")
-      .setURL("https://hyakanime.fr/agenda")
-      .setAuthor({
-        name: "hyakanime",
-        iconURL:
-          "https://www.hyakanime.fr/static/media/appLogo.7fac0ec4359bda8ccf0f.png",
-        url: "https://hyakanime.fr",
-      })
-      .setTimestamp();
-      i=0
-      while (i < nom.length) {
-        exampleEmbed.addFields({name: timestamp[i], value: nom[i].slice(0, 43)+" - "+episode[i], inline: false})
-      i++;
-      }
-        
-      
-    await interaction.editReply({ embeds: [exampleEmbed] });
-  }
 };
