23016984138538ae78cfc3e3a965bbb99806e86c
dem0nx309 <41202006+dem0nx309@users.noreply.github.com>
2023-11-19 12:47:47 +0100

    Réajout de la gestion d'erreur

---
+++ events/InteractionCreate.js
@@ -0,0 +1,25 @@
+const { Events } = require('discord.js');
+const { roleMembre, rolePatchNotes, roleIos, roleAndroid, roleSite , roleBonPlan, roleGenshin} = require('../config.json');
+
+module.exports = {
+	name: Events.InteractionCreate,
+	async execute(interaction) {
+		if (interaction.isChatInputCommand()){
+
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
+
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(`Error executing ${interaction.commandName}`);
+			console.error(error);
+            await interaction.reply({ content: 'Il y a eu une erreur lors de l’exécution de cette commande !', ephemeral: true });
+		}
+	}
+	},
+};
