bf9f8e86c30673f4c905dadad44c3093e5b50163
zaphir <ianice.lange@supdevinci-edu.fr>
2025-06-08 22:31:56 +0200

    Suppression des caractère spéciaux

--- events/MessageIncludeFeur.js
+++ events/MessageIncludeFeur.js
@@ -3,7 +3,8 @@ const { Events, GatewayIntentBits } = require("discord.js");
 module.exports = {
   name: Events.MessageCreate,
   async execute(msg) {
-    const message = msg.content ? msg.content.toLowerCase() : "";
+    let message = msg.content ? msg.content.toLowerCase() : "";
+    message = message.replace(/[^a-zA-Z0-9 ]/g, "").replace(" ", "");
     if (message.slice(-4).includes("quoi")) {
         if( Math.floor(Math.random() * 5) == 0 ) {
             msg.reply({content: "feur", allowedMentions: { repliedUser: false }}).then((m) => {
