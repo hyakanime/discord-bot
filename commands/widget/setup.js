0bdebcf146eb721a9ba6c067de8fc84672e9ed45
Julien <dem0nx309contact@gmail.com>
2026-06-12 10:39:54 +0200

    Update widget setup command to link to tutorial and improve description

--- commands/widget/setup.js
+++ commands/widget/setup.js
@@ -1,18 +1,21 @@
-const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
-const {logoUrl, urlEndpoint, oauth2} = require("../../config.json");
+const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
+const { logoUrl } = require("../../config.json");
+
+const tutoUrl = "https://widget-hyak.zaphir21.dev/";
+
 module.exports = {
     async execute(interaction) {
         const button = new ButtonBuilder()
-            .setLabel("Accéder au widget")
-            .setURL(oauth2)
+            .setLabel("Voir le tutoriel")
+            .setURL(tutoUrl)
             .setStyle(ButtonStyle.Link);
         const embed = new EmbedBuilder()
             .setTitle("Widget Hyak")
-            .setDescription("Clique sur le bouton ci-dessous pour accéder au widget Hyak. Le widget est affiché que sur ordinateur/navigateur. \n\nQuand vous aurez accepté le widget, il faut faire la commande /widget refresh")
+            .setDescription("L'ajout du widget n'est plus possible directement via Discord. Clique sur le bouton ci-dessous pour suivre le tutoriel et configurer le widget Hyak sur ton profil.")
             .setThumbnail(logoUrl);
         const row = new ActionRowBuilder().addComponents(button);
 
-        await interaction.reply({ embeds: [embed], components: [row], flags: 64});
+        await interaction.reply({ embeds: [embed], components: [row], flags: 64 });
     }
 };
 
